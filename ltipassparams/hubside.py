b32cff1a01f8713970d28fefe4679652f310fb50
Jason Mansour <jason.mansour@gwdg.de>
2022-03-02 08:45:13 +0100

    Parse the nbgitpuller link to get the launched file

--- ltipassparams/hubside.py
+++ ltipassparams/hubside.py
@@ -5,6 +5,7 @@
 
 from typing import Dict
 from tljh.user_creating_spawner import UserCreatingSpawner
+import urllib.parse
 
 import logging
 log = logging.getLogger("LtiUserCreatingSpawner")
@@ -39,3 +40,43 @@ class LtiUserCreatingSpawner(UserCreatingSpawner):
         self.auth_state = auth_state
         # should this go here, or in pre_spawn_start?
         self.environment['DEMO_FULL_NAME'] = auth_state['lis_person_name_full']
+
+        log.info("Trying to get the next link")
+        try:
+            custom_next = auth_state['custom_next']
+            parsed = parse_nbgitpuller_link(custom_next)
+            log.info("Parsed: %r", parsed)
+        except KeyError:
+            return
+
+
+def parse_nbgitpuller_link(next_url: str):
+    """ Parses the LTI next_url parameter.  If it is an nbgitpuller link,
+        it returns the details.
+
+        >>> parse_next_url("http://c106-190.cloud.gwdg.de/lti/redirect?next=http%3A%2F%2Fc106-190.cloud.gwdg.de%2Fhub%2Fuser-redirect%2Fgit-pull%3Frepo%3Dhttps%253A%252F%252Fgithub.com%252FKI-Campus%252FDatengeschichten%26urlpath%3Dtree%252FDatengeschichten%252F%26branch%3Dmain")
+        {'repo': 'https://github.com/KI-Campus/Datengeschichten', 'urlpath': 'tree/Datengeschichten/', 'branch': 'main'}
+    """
+    
+    # Unpack redirection
+    parts = urllib.parse.urlparse(next_url)
+    if parts.path == "/lti/redirect":
+        query = urllib.parse.parse_qs(parts.query)
+        try:
+            next_url = query['next'][0]
+        except KeyError:
+            pass
+
+    parts = urllib.parse.urlparse(next_url)
+    if parts.path != "/hub/user-redirect/git-pull":
+        return None
+
+    query = urllib.parse.parse_qs(parts.query)
+    try:
+        repo = query['repo'][0]
+        urlpath = query['urlpath'][0]
+        branch = query['branch'][0]
+    except KeyError:
+        return None
+
+    return {'repo': repo, 'urlpath': urlpath, 'branch': branch}
