d0cd0a4bccb7f14a3c296a4e2ceca69abd10b47c
Jason Mansour <jason.mansour@gwdg.de>
2022-06-29 10:06:21 +0200

    Move grading service to a module

--- config/jupyterhub_config.d/ltipassparams_grading.py
+++ config/jupyterhub_config.d/ltipassparams_grading.py
@@ -5,6 +5,6 @@ c.JupyterHub.services = [
     {
         'name': 'grading-service',
         'url': 'http://127.0.0.1:10101/',
-        'command': [sys.executable, '/home/cloud/src/ltipassparams/grading-service/grading-service.py'],
+        'command': [sys.executable, '-m', 'ltipassparams.grading.service'],
     },
 ]
